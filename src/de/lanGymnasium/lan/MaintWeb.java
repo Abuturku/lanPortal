921c973ecba29523cfa4a86db4765f511314a3de
Bean900 <n.nikisch@gmail.com>
2015-12-22 12:50:18 +0100

    So jetzt klappt es
    Also änderungen auf die anderen dinger übernommen

---
+++ src/de/lanGymnasium/lan/MaintWeb.java
@@ -0,0 +1,32 @@
+package de.lanGymnasium.lan;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import de.lanGymnasium.datenstruktur.User;
+
+@SuppressWarnings("serial")
+public class MaintWeb extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		User user = LoginChecker.checkLogin(req, resp);
+		if (user == null) {
+			resp.getWriter().write("<p>Sie sind nicht im System registriert! Bitte wenden Sie sich an einen Administrator!</p><br> " 
+					+ "<p><a href=\"login\">Zurück</p>");
+			//Hier kommt noch der Redirect zu Lindas Login Denied Seite.
+		}
+		req.getRequestDispatcher("maint.html").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("bin im logout");
+		LoginChecker.logout(req, resp);
+	}
+
+}
